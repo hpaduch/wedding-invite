c4c1cde2448845bf8ec54427de0fe31e1844b975
AP-PF5B9JPQ\Hari Paduchuri <hari.paduchuri@veeva.com>
2026-07-22 12:21:31 +0530

    Small typo

--- src/components/EventCards.jsx
+++ src/components/EventCards.jsx
@@ -12,7 +12,7 @@ export default function EventCards() {
       time: "7:00 PM",
       venue: "Dwaraka Convention Hall",
       desc: "A joyous evening of music, laughter, and heartfelt blessings. We invite you to share in our happiness as we embark on this new chapter of togetherness.",
-      mapsUrl: "https://www.google.com/maps/place/Dwaraka+Chalama+Reddy+Coventional+Hall",
+      mapsUrl: "https://www.google.com/maps/place/Dwaraka+Chalama+Reddy+Conventional+Hall",
       calligraphyFont: '"Pinyon Script", cursive',
       bgImage: "/Reception.jpg"
     },
@@ -25,7 +25,7 @@ export default function EventCards() {
       time: "10:30 AM",
       venue: "Dwaraka Convention Hall",    
       desc: "Witness our sacred union as we perform the traditional rituals, pledging our lives to each other amidst the holy chants of the Muhurtham.",
-      mapsUrl: "https://www.google.com/maps/place/Dwaraka+Chalama+Reddy+Coventional+Hall",
+      mapsUrl: "https://www.google.com/maps/place/Dwaraka+Chalama+Reddy+Conventional+Hall",
       calligraphyFont: '"Great Vibes", cursive',
       bgImage: "/wedding-bg.jpg"
     }
